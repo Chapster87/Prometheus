7a91f5964929c1cef118fa9e543ddc0f8960ba28
Andy Chapman <achapm87@gmail.com>
2024-02-27 22:20:35 -0500

    Create utils.js file

---
+++ utils/utils.js
@@ -0,0 +1,14 @@
+export function optimizeName(name) {
+  name = name.replace("\r", "").replace(/(-\s*\d{2,4})|vod|fhd|hd|360p|4k|h264|h265|24fps|60fps|720p|1080p|vod|x264|x265|\.avi|\.mp4|\.mkv|\[.*]|\(.*\)|\{.*\}|-|_|\./gim, " ").replace(/(- \d\d\d\d$)/, "");
+  name = name.replace("   ", " ");
+  name = name.replace("  ", " ");
+  return name.trim();
+}
+
+export function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+  return `${h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+}
